d4300e47fed81a6ed1158bf0300b077249869a46
Jee Park <jpa239@cs18269kq.canterbury.ac.nz>
2022-10-11 16:31:23 +1300

    Implementing the stages in our game. The start stage is almost complete.

--- comment.c
+++ comment.c
@@ -10,12 +10,18 @@
 void comment_init (void)
 {
     tinygl_init(PACER_RATE);
-}
-
-void intro_com (void) {
     tinygl_font_set(&font5x5_1);
     tinygl_text_speed_set(MESSAGE_RATE);
     tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
     tinygl_text_dir_set(TINYGL_TEXT_DIR_ROTATE);
+}
+
+void intro_com (void) {
     tinygl_text("  Push to start");
+}
+
+void score (char score1, char score2) 
+{
+    tinygl_point_t point = {0, 0};
+    tinygl_draw_message("score1 : score2", point, 1);
 }
---
+++ stage.h
@@ -0,0 +1,10 @@
+#ifndef STAGE_H
+#define STAGE_H
+
+game_stage_t start_stage(void);
+
+game_stage_t playing_stage(void);
+
+game_stage_t end_stage(void);
+
+#endif
